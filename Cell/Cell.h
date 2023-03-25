261844f6bbe9d647928671506886f896dc9bbf6e
Abdussalam Nikkerdar <salamnik42@gmail.com>
2023-03-25 21:16:40 +0430

    Cell header

---
+++ Cell/Cell.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Genom.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <utility>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+#define SIZE 100000 + 1
+extern unordered_map<char, char> complement;
+extern int P[SIZE * 2];
+class Cell
+{
+public:
+    Cell(int chromosomeNumber, string filename);
+
+    void removeIncorrectPairs();
+
+    string complementary(string s);
+
+    vector<int> computeLPS(string pattern);
+
+    void translocationMutation(string S1, int n, string S2, int m);
+
+    void inversionMutation(string s1, int chrom_num);
+
+    string convertToNewString(const string &s);
+
+    void findComplementaryPalindromes(int m);
+
+    void shortMutation(char o, char r, int num, int chorm_num);
+
+private:
+    vector<pair<int, Genom>> chromosomes;
+    int chromosomeNumber;
+};
