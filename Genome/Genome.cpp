7aa4217c9fd3bc3bfc716619f679bd76d836a8af
TheRipper83 <115469517+TheRipper83@users.noreply.github.com>
2023-04-07 20:09:32 +0330

    Update Genome.cpp

--- Genome/Genome.cpp
+++ Genome/Genome.cpp
@@ -1,7 +1,15 @@
-#include "Genome.h"
+   #include "Genome.h"
+   using namespace std;
+   Genome::Genome(){
+		cin>>RNA;
+		cin>>DNA[0]>>DNA[1];
+   }
+   string* Genome::getDNA(){
+	return DNA;
+   }
 	void Genome::recieve(){
 		cin>>RNA;
-		cin>>inputDNA[0]>>inputDNA[1];
+		cin>>DNA[0]>>DNA[1];
 	}
 	// tabe bara sakht mokamel
 	char Genome::help(char& a){
@@ -11,31 +19,112 @@
 			return 'A';
 		else if (a=='C')
 			return 'G';
-		else if (a=='G')
+		else
 			return 'C';
+
 	}
 	// tabe sakht DNA
 	void Genome::build(){
-		DNA[0]=RNA;
+		inputDNA[0]=RNA;
 		for (int i=0 ; i<RNA.length(); i++){
-			
+
 			if (RNA[i]=='A')
-				DNA[1][i]='T';
+				inputDNA[1][i]='T';
 			else if (RNA[i]=='T')
-				DNA[1][i]='A';
+				inputDNA[1][i]='A';
 			else if (RNA[i]=='C')
-				DNA[1][i]='G';
+				inputDNA[1][i]='G';
 			else if (RNA[i]=='G')
-				DNA[1][i]='C';
+				inputDNA[1][i]='C';
 		}
 		for (int i=0 ; i<RNA.length(); i++){
-			cout<<DNA[1][i];
+			cout<<inputDNA[1][i];
 		}
 		cout<<endl;
 	}
 	// tabe baraye jahesh koochak
 	void Genome::smallJump(char a,char b, int n){
 		int check=0;
+		for (int i=0 ; i<RNA.length();i++){
+			if (check==n)
+				break;
+			if (RNA[i]==a){
+				RNA[i]=b;
+				check++;
+			}
+		}
+		check=0;
+		char mokamel=help(b);
+		for (int i=0 ; i<DNA[0].length(); i++){
+			if (check==n)
+				break;
+			if (DNA[0][i]==a){
+				DNA[0][i]=b;
+				DNA[1][i]=mokamel;
+				check++;
+			}
+			if (check==n)
+				break;
+			if (DNA[1][i]==a){
+				DNA[1][i]=b;
+				DNA[0][i]=mokamel;
+				check++;
+			}
+		}
+		cout<<DNA[0]<<endl<<DNA[1]<<endl;
+
+	}
+
+	// tabe jahesh bozorg
+	void Genome::bigJump(string s1,string s2){
+		auto loc = RNA.find(s1);
+		for (int i=0 ; i<s1.length(); i++){
+			RNA[i+loc]=s2[i];
+		}
+		auto loc2 = DNA[0].find(s1);
+		auto loc3=DNA[1].find(s1);
+		if ( loc2>=loc3 )
+		{
+		    for (int i=0 ; i<s1.length(); i++){
+				DNA[0][i+loc2]=s2[i];
+			}
+			for (int i=0 ; i<s1.length(); i++){
+				DNA[1][i+loc2]=help(s2[i]);
+			}
+		}
+		else
+		{
+		   for (int i=0 ; i<s1.length(); i++){
+				DNA[1][i+loc3]=s2[i];
+			}
+			for (int i=0 ; i<s1.length(); i++){
+				DNA[0][i+loc3]=help(s2[i]);
+			}
+		}
+		cout<<DNA[0]<<endl<<DNA[1]<<endl;
+	}
+	// tabe baraye jahesh makoos
+	void Genome::reversing(string s){
+		auto loc = RNA.find(s);
+		if ( loc != string::npos )
+		{
+		   reverse(RNA.begin() + loc, RNA.begin() + loc +s.length());
+		}
+		auto loc2 = DNA[0].find(s);
+		auto loc3=DNA[1].find(s);
+		if ( loc2>=loc3 )
+		{
+		   reverse(DNA[0].begin() + loc2, DNA[0].begin() + loc2 +s.length());
+		   reverse(DNA[1].begin() + loc2, DNA[1].begin() + loc2 +s.length());
+		}
+		else
+		{
+		   reverse(DNA[0].begin() + loc3, DNA[0].begin() + loc3 +s.length());
+		   reverse(DNA[1].begin() + loc3, DNA[1].begin() + loc3 +s.length());
+		}
+		cout<<DNA[0]<<endl<<DNA[1]<<endl;
+	}
+
 		for (int i=0 ; i<RNA.length();i++){
 			if (check==n)
 				break;
