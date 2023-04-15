becbaf71280bba850e86dd9b9119bc14051c2f1c
Abdussalam Nikkerdar <salamnik42@gmail.com>
2023-04-15 22:19:20 +0430

    Last edition

--- Functions/Functions.cpp
+++ Functions/Functions.cpp
@@ -1,6 +1,6 @@
 
 #include "Functions.h"
-unordered_map<char, char> complement = {{'A', 'T'}, {'C', 'G'}, {'G', 'C'}, {'T', 'A'}};
+unordered_map<char, char> complement = {{'A', 'T'}, {'C', 'G'}, {'G', 'C'}, {'T', 'A'}, {'#', '#'}};
 // Method to find the complementary sequence of a given DNA sequence
 string complementary(string s)
 {
@@ -38,4 +38,90 @@ vector<int> computeLPS(string pattern)
     }
   }
   return lps;
+}
+
+int KMP(string S1, string *DNA1)
+{
+  // Find the first occurrence of S1 in DNA1 using the KMP algorithm
+  int n1 = DNA1[0].size(), m1 = S1.size();
+  vector<int> lps1 = computeLPS(S1);
+  string _S1 = complementary(S1);
+  bool found1 = false, found2 = false;
+  int i = 0, j = 0, r = 0, p = 0, s = 0;
+  while (i < n1 || p < n1)
+  {
+    if (S1[j] == DNA1[0][i])
+    {
+      i++;
+      j++;
+    }
+    if (S1[r] == DNA1[1][p])
+    {
+      p++;
+      r++;
+    }
+    if (j == m1)
+    {
+      s = i - j;
+      found1 = true;
+      break;
+    }
+    else if (i < n1 && S1[j] != DNA1[0][i])
+    {
+      if (j != 0)
+        j = lps1[j - 1];
+      else
+        i++;
+    }
+    if (r == m1)
+    {
+      s = p - r;
+      found1 = true;
+      break;
+    }
+    else if (p < n1 && S1[r] != DNA1[1][p])
+    {
+      if (r != 0)
+        r = lps1[r - 1];
+      else
+        p++;
+    }
+  }
+  if (found1)
+    return s;
+  else
+    return -1;
+}
+
+bool findWithKMP(string S1, string RNA)
+{
+  int i = 0, j = 0, s2 = 0;
+  bool found1 = false;
+  int m1 = S1.size(), n1 = RNA.size();
+  vector<int> lps1 = computeLPS(S1);
+  while (i < n1)
+  {
+    if (S1[j] == RNA[i])
+    {
+      i++;
+      j++;
+    }
+    if (j == m1)
+    {
+      s2 = i - j;
+      found1 = true;
+      break;
+    }
+    else if (i < n1 && S1[j] != RNA[i])
+    {
+      if (j != 0)
+        j = lps1[j - 1];
+      else
+        i++;
+    }
+  }
+  if (found1)
+    return true;
+  else
+    return false;
 }
--- Functions/Functions.h
+++ Functions/Functions.h
@@ -8,4 +8,9 @@ using namespace std;
 extern unordered_map<char, char> complement;
 // Method to find the complementary sequence of a given DNA sequence
 string complementary(string s);
+
 vector<int> computeLPS(string pattern);
+//KMP algorithm for find first occurrence
+int KMP(string S1,string* DNA1);
+//KMP algorithm for find first occurrence
+bool findWithKMP(string S1,string RNA);
